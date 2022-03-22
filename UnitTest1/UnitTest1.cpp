3d18e3c82957335673180add71111a5b7882b2e8
MarshmallowSoup <lysetskyyv@gmail.com>
2022-03-22 11:59:59 +0200

    initial commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab4.2/lab4.2.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:		
+		TEST_METHOD(TestMethod1)
+		{
+			string s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+			string result = findLastAWord(s);
+			string expected = "adipiscing";
+			Assert::AreEqual(expected, result);
+		}
+	};
+}
---
+++ lab4.2/lab4.2.cpp
@@ -0,0 +1,61 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+using namespace std;
+string findLastAWord(const string s)
+{
+	string str = s;
+	int a = 0;
+	int tmp = 0;
+	for (int i = str.size(); i >= 0; i--) {
+		if (str[i] == ' ') {
+			if (str[i + 1] == 'a') {
+				a = i + 1;
+				int len1 = i;
+				str.erase(0, len1+1);
+				for (int j = str.size(); j >= 0; j--) {
+					if (str[j] == ' ') {
+						int len2 = str.size() - j;
+						str.erase(j, len2+1);
+						break;
+					}
+				}
+				break;
+			}
+		}
+	}
+	return str;
+}
+
+string readFile(string path) {
+	string s;
+	getline(ifstream(path), s, '\0');
+	return s;
+}
+
+void edit(string filename) {
+	string fileContent = readFile(filename);
+
+	string result = findLastAWord(fileContent);
+	
+	fstream f(filename, ios_base::out);
+
+    if (f.is_open()){
+		f << result;
+		f.close();
+	}
+}
+
+
+int main()
+{
+	string filename = "1.txt";
+	edit(filename);
+	return 0;
+
+}
+
+
